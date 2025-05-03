a612b5282f2769432de7f8dc09f1db240c235d8e
Amanda Persdotter <amer1301@student.miun.se>
2025-05-03 07:06:32 +0200

    jwt

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,6 +4,7 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 // Connect to MongoDB
@@ -59,7 +60,14 @@ router.post("/login", async(req, res) => {
         if(!isPasswordMatch) {
             return res.status(401).json({ error : "Incorrect username/password!" });
         } else {
-            res.status(200).json({message: "User logged in!" });
+            // create JWT
+            const payload = { username: username };
+            const token = jwt.sign(payload, process.env.JWT_SECRET_KEY, { expiresIn: '1h' });
+            const response = {
+                message: "User logged in!",
+                token: token
+            }
+            res.status(200).json({ response });
         }
 
     } catch(error) {
--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const authRoutes = require("./routes/authRoutes");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const app = express();
@@ -12,6 +13,25 @@ app.use(bodyParser.json());
 
 // Routes
 app.use("/api", authRoutes);
+
+// Protected routes
+app.get("/api/protected", authenticateToken, (req, res) => {
+    res.json({ message: "Skyddad route!"});
+});
+
+// Validate Token
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1]; // Token
+    if(token == null) res.status(401).json({ message: "Not authorized for this route - token missing!"});
+
+    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, username) => {
+        if(err) return res.status(403).json({ message: "Not valid JWT"});
+
+        req.username = username;
+        next();
+    });
+}
 
 // Starta applikationen
 app.listen(port, () => {
